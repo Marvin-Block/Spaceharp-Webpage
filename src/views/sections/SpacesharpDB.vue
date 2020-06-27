fa615fcc3fcaca779e39cbd5ca24a05f53e3e58f
unknown <marvin.block1998@gmail.com>
2020-06-27 14:14:20 +0200

    saving ecnrypted files as .sse

--- src/views/sections/SpacesharpDB.vue
+++ src/views/sections/SpacesharpDB.vue
@@ -375,7 +375,12 @@
       forceFileDownload (response) {
         const url = window.URL.createObjectURL(new Blob([response.data.file]))
         const link = document.createElement('a')
-        const filenName = response.data.name + '.sss'
+        let filenName
+        if (response.data.encrypted) {
+          filenName = response.data.name + '.sse'
+        } else {
+          filenName = response.data.name + '.sss'
+        }
         link.href = url
         link.setAttribute('download', filenName)
         document.body.appendChild(link)
